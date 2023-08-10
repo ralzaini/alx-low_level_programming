b11555959c2179e04327858d11b18855b678e67d
ralzaini <rehabalzaini@gmail.com>
2023-08-10 08:54:03 -0700

    All files

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/*
+ * main  - Entry Point
+ *
+ * Desription: a script that returns the size of various feilds
+ *
+ * Return -Always 0 (success)
+ */
+
+int main(void)
+{
+	printf("Size of a char: %u bytes(s)\n", sizeof(char));
+	printf("Size of an int: %u bytes(s)\n", sizeof(int));
+	printf("Size of a long int: %u bytes(s)\n", sizeof(long int));
+	printf("Size of a long long int: %u bytes(s)\n", sizeof(long long int));
+	printf("Size of a float: %u bytes(s)\n", sizeof(float));
+	return (0);
+}

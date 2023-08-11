261cc1481e1c269c09be8d04d8388ba1f7e4f49a
ralzaini <rehabalzaini@gmail.com>
2023-08-11 16:58:09 -0700

    advanced 1

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,7 +10,28 @@
 
 int main(void)
 {
-	int x,
+	int x = 0;
+	int y = 0;
 
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = x + 1; y <= 9; y++)
+		{
+			if (!(x > y) || x == y)
+			{
+				putchar('0' + x);
+				putchar('0' + y);
+				if (x == 8 && y == 9)
+				{
+					putchar('\n');
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
 	return (0);
 }

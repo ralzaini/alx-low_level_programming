be4a7c3bcd6da323a92be054e48994fa373cb7e8
ralzaini <rehabalzaini@gmail.com>
2023-08-15 04:36:19 -0700

    q

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -11,14 +11,14 @@
 
 int main(void)
 {
-	int i = 0;
-	char put[] = "_putchar";
-
-	while (i < 9)
-	{
-		_putchar(put[i]);
-		i++;
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 
 	return (0);

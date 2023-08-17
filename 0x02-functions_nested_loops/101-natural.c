f4b6528274ee3f1512d0d670bc50a949fab66475
ralzaini <rehabalzaini@gmail.com>
2023-08-17 03:38:10 -0700

    r

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -10,13 +10,9 @@ int main(void)
 {
 	int sum, i = 0;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i < 1024; i++)
 	{
-		if ((i % 3) == 0)
-		{
-			sum = sum + i;
-		}
-		if ((i % 5) == 0)
+		if ((i % 3) == 0 || (i % 5) == 0)
 		{
 			sum = sum + i;
 		}

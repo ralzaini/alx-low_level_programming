9837dbcac9bc5716ac478961ec2a75baf28501c3
ralzaini <rehabalzaini@gmail.com>
2023-08-17 02:59:26 -0700

    o

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -14,33 +14,29 @@ void print_times_table(int n)
 	{
 		for (i = 0; i <= n; i++)
 		{
-			for (j = 0; j <= n; j++)
+			_putchar('0');
+			for (j = 1; j <= n; j++)
 			{
+				_putchar(',');
+				_putchar(' ');
 				k = i * j;
-				if (k > 99)
+				if (k <= 9)
 				{
-					_putchar('0' + k / 100);
-					_putchar('0' + (k / 10 % 10));
-					_putchar('0' + k % 10);
+					_putchar(' ');
+					_putchar(' ');
+					_putchar('0' + k);
 				}
-				else if (k > 9 && k <= 99)
+				else if (k <= 99 && k > 9)
 				{
 					_putchar(' ');
 					_putchar('0' + k / 10);
 					_putchar('0' + k % 10);
 				}
-				else if (j != 0)
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar('0' + k);
-				}
 				else
-					_putchar('0' + k);
-				if (j != n)
 				{
-					_putchar(',');
-					_putchar(' ');
+					_putchar('0' + k / 100);
+					_putchar('0' + (k / 10 % 10));
+					_putchar('0' + k % 10);
 				}
 			}
 			_putchar('\n');
@@ -48,4 +44,4 @@ void print_times_table(int n)
 	}
 	else
 		return;
-}
+}	

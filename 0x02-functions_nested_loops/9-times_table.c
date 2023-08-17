c74a9a249bad50565083422234afad65059eb209
ralzaini <rehabalzaini@gmail.com>
2023-08-16 23:52:26 -0700

    i

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,26 +13,22 @@ void times_table(void)
 
 	while (i < 10)
 	{
-		j = 0;
+		j = 1;
+		_putchar('0');
 		while (j < 10)
 		{
+			_putchar(',');
+			_putchar(' ');
 			k = i * j;
-			if (k > 9)
+
+			if (k <= 9)
 			{
-				_putchar('0' + k / 10);
-				_putchar('0' + k % 10);
-			}
-			else if (j != 0)
-			{
-				_putchar(' ');
 				_putchar('0' + k);
 			}
 			else
-				_putchar('0' + k);
-			if (j != 9)
 			{
-				_putchar(',');
-				_putchar(' ');
+				_putchar('0' + k / 10);
+				_putchar('0' + k % 10);
 			}
 			j++;
 		}

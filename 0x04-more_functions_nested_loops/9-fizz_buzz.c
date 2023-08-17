1226d5b50fbc3f5754034f711f76130d7df7fc83
ralzaini <rehabalzaini@gmail.com>
2023-08-17 10:04:02 -0700

    k

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+* main - entry block
+* @void: no argument
+* Return: 0 Success
+*/
+
+int main(void)
+{
+	int i = 0;
+
+	for (i = 0; i <= 100; i++)
+	{
+		if (i != 100)
+		{
+			if ((i % 3 == 0) && (i % 5 == 0))
+			{
+				printf("FizzBuzz ");
+			}
+			else if (i % 3 == 0)
+			{
+				printf("Fizz ");
+			}
+			else if (i % 5 == 0)
+			{
+				printf("Buzz ");
+			}
+			else
+			{
+				printf("%d ", i);
+			}
+		}
+		else
+		{
+			printf("Buzz\n");
+		}
+	}
+}

ebb1ec475e808dde0898262fcad2983429e259ba
ralzaini <rehabalzaini@gmail.com>
2023-08-17 07:50:32 -0700

    p

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,21 +6,19 @@
 
 void more_numbers(void)
 {
-	int i = 0, j = 0;
+	int i, j, k;
 
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			if (j <= 9)
+			k = j;
+			if (j > 9)
 			{
-				_putchar('0' + j);
+				_putchar('0' + 1);
+				k = j % 10;
 			}
-			else
-			{
-				_putchar('0' + j / 10);
-				_putchar('0' + j % 10);
-			}
+			_putchar('0' + k);
 		}
 		_putchar('\n');
 	}

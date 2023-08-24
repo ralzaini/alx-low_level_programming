b0c68997b94fc335f4603c7b8b84744bec046406
ralzaini <rehabalzaini@gmail.com>
2023-08-24 04:43:22 -0700

    5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_toupper - change all lowercase to uppercase
+ * @n: pointer
+ *
+ * Return: n
+ */
+
+char *string_toupper(char *n)
+{
+	int i = 0;
+
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		if (n[i] >= 'a' && n[i] <= 'z')
+		{
+			n[i] = n[i] - 32;
+		}
+	}
+	return (n);
+}

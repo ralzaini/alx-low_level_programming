68141b9ad78d13fa91b21f5fb337465b0e6357ac
ralzaini <rehabalzaini@gmail.com>
2023-08-24 08:35:04 -0700

    8

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string in rot13
+ * @s: string to be encoded
+ *
+ * Return: the resulting string
+ */
+
+char *rot13(char *s)
+{
+	int i, j;
+	char r[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char R[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; r[j] != '\0'; j++)
+		{
+			if (s[i] == r[j])
+			{
+				s[i] = R[j];
+			}
+		}
+	}
+	return (s);
+}

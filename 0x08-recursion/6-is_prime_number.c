84692940e5b9e29b7f3a5bfcca6e989ec82b0f9c
ralzaini <rehabalzaini@gmail.com>
2023-08-30 13:19:55 -0700

    6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,29 +8,7 @@
 
 int is_prime_number(int n)
 {
-	if (n < 2)
-		return (0);
-	if (n < 4)
-		return (1);
 	return (c_prime(n, 2));
-}
-
-/**
- * _sqrt - return square root of number
- * @x: number
- * @i: number incrementer acting as divisor
- * Return: square root of `x`
- */
-
-int _sqrt(int j, int k)
-{
-	int s;
-
-	s = k * k;
-	if (s >= j)
-		return (k);
-	else
-		return (_sqrt(j, k + 1));
 }
 
 /**
@@ -42,10 +20,10 @@ int _sqrt(int j, int k)
 
 int c_prime(int n, int i)
 {
-	if (n % i == 0)
-		return (0);
-	else if (_sqrt(n, 1) < i)
+	if (i >= n && n > 1)
 		return (1);
+	else if (n % i == 0 || n <= 1)
+		return (0);
 	else
 		return (c_prime(n, i + 1));
 }

8796fbb8f085d2a0460085f356be76851b835872
ralzaini <rehabalzaini@gmail.com>
2023-09-01 09:08:27 -0700

    3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main -  writes its name
+ * @argc: arguement count
+ * @argv: array of pointers to arguement strings
+ * Return: 0
+ **/
+
+int main(int argc, char *argv[])
+{
+	argc = argc;
+	if (argv[1] && argv[2])
+	{
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	}
+	else
+	{
+		printf("Error");
+	}
+	return (0);
+}

dee01fe87f5b2d1b4385d16edeea35a1e6fdc02c
ralzaini <rehabalzaini@gmail.com>
2023-09-06 02:09:27 -0700

    again

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,42 +1,27 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
-
-/**
- * letter_count - count num of words recursively
- * @str: pointer to char
- * @i: current index
- * Return: number of words
- **/
-
-int letter_count(char *str, int i)
-{
-	if (str[i] == '\0')
-	{
-		return (0);
-	}
-	if (str[i] == ' ' && str[i + 1] != '\0' && str[i + 1] != ' ')
-	{
-		return (1 + letter_count(str, i + 1));
-	}
-	return (letter_count(str, i + 1));
-}
-
 /**
  * w_count - counts number of words in 1d array of strings
  * @str: pointer to char
  * Return: number of words
  **/
-
 int w_count(char *str)
 {
-	if (str[0] != ' ')
-	{
-		return(1 + letter_count(str, 0));
-	}
-	return (letter_count(str, 0));
-}
+	int i, n = 0;
 
+	for (i = 0; str[i]; i++)
+	{
+		if (str[i] == ' ')
+		{
+			if (str[i + 1] != ' ' && str[i + 1] != '\0')
+				n++;
+		}
+		else if (i == 0)
+			n++;
+	}
+	n++;
+	return (n);
+}
 /**
  * strtow - splits a string into words.
  * @str: string to be splitted
@@ -44,54 +29,43 @@ int w_count(char *str)
  **/
 char **strtow(char *str)
 {
-	char **n;
-	int i = 0, j, k, l;
+	char **c;
+	int i = 0, j, k, l, w = 0, n = 0;
 
-	if (str == NULL || str[0] == 0)
-	{
+	if (str == NULL || *str == '\0')
 		return (NULL);
-	}
-	l = w_count(str);
-	if (l < 1)
-	{
+	n = w_count(str);
+	if (n == 1)
 		return (NULL);
-	}
-	n = malloc(sizeof(char *) * (l + 1));
-	if (n == NULL)
-	{
+	c = (char **)malloc(sizeof(char *) * n);
+	if (c == NULL)
 		return (NULL);
-	}
-	while ( i < l && *str != '\0')
+	c[n - 1] = NULL;
+	while (str[i])
 	{
-		if (*str == ' ')
+		if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
 		{
-			j = 0;
-			while (str[j] != ' ')
+			for (j = 1; str[i + j] != ' ' && str[i + j]; j++)
+				;
+			j++;
+			c[w] = (char *)malloc(sizeof(char) * j);
+			j--;
+			if (c[w] == NULL)
 			{
-				j++;
-			}
-			n = malloc(sizeof(char) * (j + 1));
-			if (n[i] == NULL)
-			{
-				while (--i >= 0)
-				{
-					free(n[--i]);
-				}
-				free(n);
+				for (k = 0; k < w; k++)
+					free(c[k]);
+				free(c[n - 1]);
+				free(c);
 				return (NULL);
 			}
-			k = 0;
-			while (k < j)
-			{
-				n[i][k] = *str;
-				k++;
-				str++;
-			}
-			n[i][k] = '\0';
-			i++;
+			for (l = 0; l < j; l++)
+				c[w][l] = str[i + 1];
+			c[w][l] = '\0';
+			w++;
+			i += j;
 		}
-		str++;
+		else
+			i++;
 	}
-	n[i] = '\0';
-	return (n);
+	return (c);
 }

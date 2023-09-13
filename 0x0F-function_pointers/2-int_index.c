d867e954f338ec7b6de52fc272989c112893c84c
ralzaini <rehabalzaini@gmail.com>
2023-09-13 10:28:01 -0700

    2m

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,12 +14,14 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (array == NULL && size <= 0 && cmp == NULL)
 		return (-1);
-
-	for (i = 0; i < size; i++)
+	else
 	{
-		if (cmp(array[i]) != 0)
+		for (i = 0; i < size; i++)
 		{
-			return (i);
+			if (cmp(array[i]) != 0)
+			{
+				return (i);
+			}
 		}
 	}
 	return (-1);

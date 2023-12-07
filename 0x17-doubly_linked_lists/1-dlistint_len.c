af59879b9cd30411aa80bed1c5982368c819327a
ralzaini <rehabalzaini@gmail.com>
2023-12-07 12:15:06 -0800

    ss

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * dlistint_len - number of elements in doubly linked list
+ * @h: header
+ *
+ * Return: number of nodes, 0 if empty
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+    size_t i = 0;
+
+    while (h)
+    {
+        h = h->next;
+        i++;
+    }
+    return (i);
+}

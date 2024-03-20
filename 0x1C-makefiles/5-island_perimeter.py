9a097fcaadd52dff30dfd734fdf0ca3b322229a2
ralzaini <rehabalzaini@gmail.com>
2024-03-21 01:06:44 +0300

    ilos

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -6,18 +6,16 @@ def island_perimeter(grid):
     """Island perimerter"""
     perimeter = 0
     height = len(grid)
-    if len(grid[0]):
-        width = len(grid[0])
-    for i, row in enumerate(grid):
-        for j, element in enumerate(row):
-            """Count number of perimeter"""
-            if element == 1:
-                if i - 1 >= 0 and grid[i-1][j] == 0 or i - 1 < 0:
-                    perimeter += 1
-                if j - 1 >= 0 and grid[i][j-1] == 0 or j - 1 < 0:
-                    perimeter += 1
-                if i + 1 < height and grid[i+1][j] == 0 or i + 1 >= height:
-                    perimeter += 1
-                if j + 1 < width and grid[i][j+1] == 0 or j + 1 >= width:
-                    perimeter += 1
+    width = len(grid[0]) if height > 0 else 0
+    
+    for i in range(height):
+        for j in range(width):
+            if grid[i][j] == 1:
+                perimeter += 4
+                
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2  
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+                
     return perimeter
